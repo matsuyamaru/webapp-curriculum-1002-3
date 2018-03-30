091a41a236412cb221fbc6a7be0e2a68c7fdac66
松山　龍 <matsuyamaryuu@Mac-mini-ryu-288.local>
2018-03-30 18:13:52 +0900

    末尾再帰最適化を行い、スタックオーバーフローになることを防ぐ

--- Factorial.scala
+++ Factorial.scala
@@ -1,7 +1,11 @@
 import scala.math.BigInt
 
 object Factorial extends App {
-  def factorial(i: BigInt): BigInt = if (i == 0) 1 else i * factorial(i - 1)
-
-  println(factorial(10000))
+  def factorial(i: BigInt,m: BigInt): BigInt = {
+    if (i <= 1) 
+      m
+    else
+      factorial(i - 1,i * m)
+  }
+  println(factorial(1000,1))
 }
